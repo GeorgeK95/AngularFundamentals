af77691643eb70dc94f71474c4afa101c5277d38
GeorgeK95 <george_it@abv.bg>
2018-07-24 19:57:35 +0300

    Bug fixed.

--- articles-app/src/data.ts
+++ articles-app/src/data.ts
@@ -4,13 +4,12 @@ import data from './seed';
 export default class ArticleData {
   getData(): Article[] {
     const articles: Article[] = [];
-
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.data.length; i++) {
       const currentArticle = new Article(
-        data[i].title,
-        data[i].description,
-        data[i].author,
-        data[i].imageUrl
+        data.data[i].title,
+        data.data[i].description,
+        data.data[i].author,
+        data.data[i].imageUrl
       );
 
       articles[i] = currentArticle;
